7ed3c61df6f3caa4ba3755e0d4a15d706d0824b0
korrr <ccorina.ungur@gmail.com>
2015-06-13 16:09:21 +0300

    unfunctional test

--- client/testagent.py
+++ client/testagent.py
@@ -6,6 +6,7 @@ class test_agent(object):
 
 	def test_all(self):
 		self.test_printstatistics_callsonconnector()
+#		self.test_senddata_callsonconnector()
 
 
 	def test_printstatistics_callsonconnector(self):
@@ -17,3 +18,22 @@ class test_agent(object):
 
 		agent.connector.request_data.assert_called_once_with("name", d)
 		agent.connector.get_last_message.assert_called_once_with()
+
+# 	def test_senddata_callsonconnector(self):
+# 		agent = Agent("test2")
+# 		agent.connector = Mock()
+# 		freespace = 2345
+# 		load = 99
+# 		test = Test()
+# 		test.LoadPercentage = freespace
+# 		test.FreeSpace = load
+# 		vect = [test]
+# 		agent.w.Win32_LogicalDisk = MagicMock(vect)
+# 		agent.w.Win32_Processor = MagicMock(vect)
+# 		agent.send(1,1)
+# 		data = {"cpu_load": load, "free_space":freespace}
+# 		agent.connector.send_data.assert_called_with("test2", data)
+
+# class Test(object):
+# 	LoadPercentage = 10
+# 	FreeSpace = 20
